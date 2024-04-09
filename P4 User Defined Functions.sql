6a70522cba0613f8031b867f4bb71c7c42011dd1
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2024-04-09 18:49:08 -0400

    made changes in user defined fucntion

--- P4 User Defined Functions.sql
+++ P4 User Defined Functions.sql
@@ -1,25 +1,7 @@
 use Airhub;
 
 GO
-CREATE FUNCTION dbo.CalculateAge (@Birthdate DATE)
-RETURNS INT
-AS
-BEGIN
-    RETURN (SELECT DATEDIFF(YEAR, @Birthdate, GETDATE()) - CASE
-                WHEN MONTH(@Birthdate) > MONTH(GETDATE()) OR (MONTH(@Birthdate) = MONTH(GETDATE()) AND DAY(@Birthdate) > DAY(GETDATE()))
-                THEN 1
-                ELSE 0
-            END);
-END;
-GO
-
-ALTER TABLE Passenger
-ADD Age AS dbo.CalculateAge(Birthdate);
-
-SELECT * FROM Passenger;
-
-GO
-CREATE FUNCTION dbo.CalculateFlightDuration (@DepartureDateTime DATETIME, @ArrivalDateTime DATETIME)
+CREATE FUNCTION dbo.CalculateFlightDuration(@DepartureDateTime DATETIME, @ArrivalDateTime DATETIME)
 RETURNS INT
 AS
 BEGIN
@@ -30,6 +12,5 @@ GO
 ALTER TABLE FlightSchedule
 ADD FlightDurationHours AS dbo.CalculateFlightDuration(DepartureTime, ArrivalTime);
 
-SELECT * FROM FlightSchedule;
 
 
